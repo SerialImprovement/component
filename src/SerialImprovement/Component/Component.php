6f281aa2d84cbd6afd1909a42d9319d9ab687d25
bigtallbill <bigtallbill@gmail.com>
2016-05-13 21:18:04 -0400

    Add first version

---
+++ src/SerialImprovement/Component/Component.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace SerialImprovement\Component;
+
+
+use SerialImprovement\Core\Input;
+
+abstract class Component
+{
+    /** @var Input */
+    protected $props;
+
+    /** @var Component[] */
+    protected $children = [];
+
+    /**
+     * Component constructor.
+     * @param $props
+     * @param array $children
+     */
+    public function __construct(Input $props = null, array $children = [])
+    {
+        // dirty default props to new standard class
+        if ($props === null){
+            $props = new Input();
+        }
+
+        $this->props = $props;
+        $this->children = $children;
+    }
+
+    protected function renderChildren()
+    {
+        $rendered = '';
+        foreach ($this->children as $child) {
+            $rendered .= $child->render();
+        }
+
+        return $rendered;
+    }
+
+    /**
+     * should render the given component
+     *
+     * @return string
+     */
+    abstract public function render();
+
+    /**
+     * Allows you to use components directly inside strings
+     * Simply calls the render method when php tries to convert to a string
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->render();
+    }
+}
